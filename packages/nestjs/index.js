046f9fb32f94df1ede08729cdc96da02f493b24b
daopk2 <daopk2@vng.com.vn>
2023-02-09 18:11:35 +0700

    updated nestjs rules

--- packages/nestjs/index.js
+++ packages/nestjs/index.js
@@ -3,6 +3,11 @@ module.exports = {
     '@daopk/eslint-config-ts',
   ],
   rules: {
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
     '@typescript-eslint/semi': ['error', 'always'],
   },
 }
