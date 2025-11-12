30bdb90858ff654d7835b23e4e42b24a6c2fde69
Dao Phan <daopk.dev@gmail.com>
2025-11-12 23:41:30 +0700

    feat: extend presetNestjs to accept user configurations

--- src/presets/nestjs.ts
+++ src/presets/nestjs.ts
@@ -1,17 +1,19 @@
 import type { LinterConfig, Options } from '../types'
 import daopk from '..'
 
-export async function presetNestjs(options: Options = {}): Promise<LinterConfig[]> {
+export async function presetNestjs(options: Options = {}, ...userConfigs: LinterConfig[]): Promise<LinterConfig[]> {
     return daopk({
         stylistic: {
             semi: true,
             jsx: false,
             ...options.stylistic,
         },
+        ...options,
+    }, {
+        name: 'daopk/typescript/nestjs',
+        files: ['**/*.ts'],
         rules: {
             'typescript/consistent-type-imports': ['warn', { prefer: 'no-type-imports' }],
-            ...options.rules,
         },
-        ...options,
-    })
+    }, ...userConfigs)
 }
